0a7213068fa7a5a99391853940bc979f001948a4
Wojciech Malarski <w.malarski@gmail.com>
2022-06-05 17:51:08 +0200

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+import tailwind from "@astrojs/tailwind";
+import turbolinks from "@astrojs/turbolinks";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), turbolinks(), sitemap()]
+});
