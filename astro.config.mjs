a4873654467838714a97a41ab6b1311c7c231534
Wojciech Malarski <w.malarski@gmail.com>
2022-06-05 18:27:06 +0200

    netlify adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import turbolinks from "@astrojs/turbolinks";
 import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), turbolinks(), sitemap()]
+  integrations: [tailwind(), turbolinks(), sitemap()],
+  adapter: netlify(),
 });
