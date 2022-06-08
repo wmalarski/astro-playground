a0fb639888f77dacac17ab64b085b804c95bef01
Wojciech Malarski <w.malarski@gmail.com>
2022-06-08 21:19:17 +0200

    mobile drawer started

---
+++ src/components/Drawer/Drawer.tsx
@@ -0,0 +1,44 @@
+import type { ComponentChildren, FunctionalComponent } from "preact";
+import { useEffect, useState } from "preact/hooks";
+import Styles from "./Drawer.module.css";
+
+type Props = {
+  children: ComponentChildren;
+  id: string;
+};
+
+const Drawer: FunctionalComponent<Props> = ({ children, id }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const callback = (e: CustomEvent<string>) => {
+      if (id !== e.detail) return;
+      setIsVisible(false);
+    };
+
+    window.addEventListener("drawer:close", callback);
+    return () => {
+      window.removeEventListener("drawer:close", callback);
+    };
+  }, [id]);
+
+  useEffect(() => {
+    const callback = (e: CustomEvent<string>) => {
+      if (id !== e.detail) return;
+      setIsVisible(true);
+    };
+
+    window.addEventListener("drawer:open", callback);
+    return () => {
+      window.removeEventListener("drawer:open", callback);
+    };
+  }, []);
+
+  return (
+    <div className={Styles.drawer} aria-hidden={!isVisible}>
+      {children}
+    </div>
+  );
+};
+
+export default Drawer;
