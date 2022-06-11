c0ed9e38a29e6a1a540aa225ae44f54129629045
Wojciech Malarski <w.malarski@gmail.com>
2022-06-11 10:34:22 +0200

    drawer loading issue fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import turbolinks from "@astrojs/turbolinks";
-import sitemap from "@astrojs/sitemap";
-
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), turbolinks(), sitemap(), preact()]
+  integrations: [tailwind(), preact()],
 });
