f9cb647ea927684e100a95f6dc069d3d47afaa58
Wojciech Malarski <w.malarski@gmail.com>
2022-06-11 21:42:59 +0200

    internalization added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    tailwind(),
+    preact(),
+    astroI18next({
+      resourcesPath: "./src/locales/",
+      i18next: { debug: true, supportedLngs: ["en", "pl"] },
+    }),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
